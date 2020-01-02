d9d22c8001dc590e5bfa3fe85d410b38bcf044d3
Allison Bradley <allison.bradley@outlook.com>
2020-01-02 15:44:21 -0700

    Added corresponding city info in onclick for Submit

--- assets/script.js
+++ assets/script.js
@@ -14,74 +14,79 @@ function onLoad() {
 
     function buttonDisplay() {
         $('#prevSearches').html("")
-    for (var i = 0; i < btnArray.length; i++) {
-        var newBtn = $(`<button id=${btnArray[i].split(" ").join("-")}>${btnArray[i]}</button>`)
-        newBtn.addClass("btn")
-        $("#prevSearches").append(newBtn)
+        for (var i = 0; i < btnArray.length; i++) {
+            var newBtn = $(`<button id=${btnArray[i].split(" ").join("-")}>${btnArray[i]}</button>`)
+            newBtn.addClass("btn")
+            $("#prevSearches").append(newBtn)
+        }
     }
-}
 
+    function weatherDisplay() {
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            fiveDayUrl = (`https://api.openweathermap.org/data/2.5/forecast?id=${response.id}&units=imperial&APPID=${apiKey}`)
+            UVindex = (`https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${response.coord.lat}&lon=${response.coord.lon}&APPID=${apiKey}`)
+            console.log(UVindex)
+            $("#temp").text(`${response.main.temp} F`)
+            $("#humidity").text(response.main.humidity)
 
-$("#submitBtn").on("click", function (event) {
-    if ($("#searchQuery").val() === "") {
-        return
-    } else {
-    event.preventDefault()
-    userQuery = ($("#searchQuery").val())
-    btnArray.push(userQuery)
-    $("#searchQuery").val("")
-    localStorage.setItem("button array", JSON.stringify(btnArray))
-    buttonDisplay()
+            $.ajax({
+                url: UVindex,
+                method: "GET"
+            }).then(function (UV) {
+                $("#uv-index").text(UV[0].value)
+            })
+
+            $.ajax({
+                url: fiveDayUrl,
+                method: "GET"
+            }).then(function (fiveDay) {
+                $("#fiveDayForecast").text("")
+                for (var j = 5; j < 40; j += 8) {
+                    console.log(fiveDay)
+                    console.log(fiveDayUrl)
+                    var fiveDayDate = `<div id="fiveDayDate">${moment(fiveDay.list[j].dt_txt).format("MMM Do, YYYY")}</div>`
+                    console.log(fiveDay.list[j].weather[0].icon)
+                    var fiveDayIcon = `<div><img src="http://openweathermap.org/img/w/${fiveDay.list[j].weather[0].icon}.png"></div>`
+                    var fiveDayTemp = `<div>Temp: ${fiveDay.list[j].main.temp} F</div>`
+                    var fiveDayHumidity = `<div>Humidity: ${fiveDay.list[j].main.humidity}%</div>`
+                    $("#fiveDayForecast").append(`<div class="futureDate">${fiveDayDate}${fiveDayIcon}${fiveDayTemp}${fiveDayHumidity}</div>`)
+                }
+            })
+
+        })
     }
-})
 
-$("#prevSearches").on("click", ".btn", function (event) {
-    event.preventDefault()
-    queryUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${this.id.trim().split("-").join("+")}&APPID=${apiKey}`)
-    console.log(queryUrl)
-    var fiveDayUrl
-    var UVindex
-    $("#city").text(this.id.split("-").join(" "))
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response) {
-        fiveDayUrl = (`https://api.openweathermap.org/data/2.5/forecast?id=${response.id}&APPID=${apiKey}`)
-        UVindex = (`https://api.openweathermap.org/data/2.5/uvi/forecast?lat=${response.coord.lat}&lon=${response.coord.lon}&APPID=${apiKey}`)
-        console.log(UVindex)
-        $("#temp").text(response.main.temp)
-        $("#humidity").text(response.main.humidity)
-        
-        $.ajax({
-            url: UVindex,
-            method: "GET"
-        }).then(function(UV) {
-            $("#uv-index").text(UV[0].value)
-        })
-        
-        $.ajax({
-            url: fiveDayUrl,
-            method: "GET"
-        }).then(function(fiveDay) {
-            $("#fiveDayForecast").text("")
-            for (var j = 5; j < 40; j+=8) {
-                console.log(fiveDay)
-                console.log(fiveDayUrl)
-                var fiveDayDate =  `<div id="fiveDayDate">${moment(fiveDay.list[j].dt_txt).format("MMM Do YYYY")}</div>`
-                console.log(fiveDay.list[j].weather[0].icon)
-                var fiveDayIcon = `<div><img src="http://openweathermap.org/img/w/${fiveDay.list[j].weather[0].icon}.png"></div>`
-                var fiveDayTemp = `<div>Temp: ${fiveDay.list[j].main.temp}</div>`
-                var fiveDayHumidity = `<div>Humidity: ${fiveDay.list[j].main.humidity}%</div>`
-                $("#fiveDayForecast").append(`<div class="futureDate">${fiveDayDate}${fiveDayIcon}${fiveDayTemp}${fiveDayHumidity}</div>`)
-            }
-        })
-        
+    $("#submitBtn").on("click", function (event) {
+        if ($("#searchQuery").val() === "") {
+            return
+        } else {
+            event.preventDefault()
+            userQuery = ($("#searchQuery").val())
+            btnArray.push(userQuery)
+            $("#searchQuery").val("")
+            localStorage.setItem("button array", JSON.stringify(btnArray))
+            buttonDisplay()
+            queryUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${userQuery.trim().split("-").join("+")}&units=imperial&APPID=${apiKey}`)
+            $("#city").text(userQuery.split("-").join(" "))
+            weatherDisplay()
+        }
     })
 
-})
+    $("#prevSearches").on("click", ".btn", function (event) {
+        event.preventDefault()
+        queryUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${this.id.trim().split("-").join("+")}&units=imperial&APPID=${apiKey}`)
+        console.log(queryUrl)
+        var fiveDayUrl
+        var UVindex
+        $("#city").text(this.id.split("-").join(" "))
+        weatherDisplay()
 
-buttonDisplay()
+    })
+
+    buttonDisplay()
 }
 
 onLoad()
